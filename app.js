a883516f38906f9e35075b14c5a20fcf617bc257
Hammam <hhammamnajemm@gmail.com>
2021-03-22 22:23:05 +0200

    on this version we add login and sign up routes, these routes for authentication and signing up

--- app.js
+++ app.js
@@ -2,7 +2,27 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-// for mysql
+
+// for passport 
+const passport = require('passport');
+app.use(passport.initialize());
+const LocalStrategy = require('passport-local').Strategy;
+
+// for joi
+const Joi = require('joi');
+const signupSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(6).required(),
+    repeat_password: Joi.ref('password'),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
+})
+const signinSchema = Joi.object({
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(6).required(),
+    repeat_password: Joi.ref('password'),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
+})
+
+// for mysql local
 const mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
@@ -11,53 +31,55 @@ var con = mysql.createConnection({
     database: "temp_schema"
 });
 
-// for passport 
-const passport = require('passport');
-app.use(passport.initialize());
-const LocalStrategy = require('passport-local').Strategy;
+// for mysql heroku database (cloud)
+// var con = mysql.createConnection({
+//     host: "us-cdbr-east-03.cleardb.com",
+//     user: "b7745ecb1ba44f",
+//     password: "3c279f25",
+//     database: "heroku_01fdc8af46fd8ee"
+// });
 
-passport.use(new LocalStrategy((userName, password, done)=>{
-    
-    const sql1 = `select * from users WHERE name= '${userName}'; `;
-    con.query(sql1, (err, user) =>{
-        if (err) return done(err);
-        if(!user[0]) return done(null, false, 'Incorrect user name.');
-        if(password != user[0].password) return done(null, false, 'Incorrect password.');
 
-        // console.log(user[0]);
-        return done(null, user[0]);
+passport.use(new LocalStrategy(
+    {   // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+    },
+    (email, password, done)=>{
+        const sql1 = `select * from users WHERE email= '${email}'; `;
+        con.query(sql1, (err, user) =>{
+            if (err) return done(err);
+            if(!user[0]) return done(null, false, 'Email not found');
+            if(password != user[0].password) return done(null, false, 'Incorrect password.');
+            return done(null, user[0]);
     }); 
 }));
 
-
 app.post('/signup', (req, res) =>{
-    try {        
-        const sql1 = `INSERT INTO users (name, password) VALUES ('${req.body.username}', '${req.body.password}')`;
-        con.query(sql1, (err, result) =>{
-            if (err) throw err;
-            console.log(result);
-            return res.send(result);
-        });
-    }catch (error) {
-        return res.status(404).send(error);
-    }
+    const {error, value} = signupSchema.validate({username: req.body.username, email: req.body.email, password: req.body.password});
+    if (error) return res.status(400).json({error: error.message});
+
+    const sql1 = `INSERT INTO users (name, email, password) VALUES ('${req.body.username}', '${req.body.email}','${req.body.password}')`;
+    con.query(sql1, (err, result) =>{
+        if (err) return res.status(400).json({error: err.sqlMessage});
+        return res.status(200).json({message: "added", id: result.insertId});
+    });
 });
 
-// app.post('/login', (req, res, next) =>{
-//     passport.authenticate('local',{session:false} ,{successRedirect:'/', failureRedirect: '/login', failureFlash: false})(req, res,next);
-// });
-
 app.post('/login', (req, res, next)=> {
+    const {error, value} = signinSchema.validate({email: req.body.email, password: req.body.password});
+    if (error) return res.status(400).json({error: error.message});
+
     passport.authenticate('local', (err, user, info) => {
       if (err) return next(err); 
-      if (!user) return res.send('not authorized')
-      return res.send('authorized');
+      if (!user) return res.json({error: 'not authorized'})
+      return res.json({message:'authorized'});
     })(req, res, next);
 });
 
 
 app.get('/', (req, res) =>{
-    res.send('Holow world??');
+    res.json({message: "Hellow world !!"});
 });
 
 app.post('/users', (req, res, next) =>{
